e9ce18b0de53e729032b51b4aeb6514382eb37d6
xia-yufeng <714811246@qq.com>
2020-10-31 00:21:55 +0800

    latest

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,17 +1,14 @@
-# TODO: import ...
 import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_random_matrix(m, n):
     return np.random.randint(0,2,(m,n))
-    # raise NotImplementedError  # TODO: 删除该行，实现该函数功能
 
 
 def save_matrix(matrix, file_name):
     plt.plot(matrix)
     plt.savefig(file_name)
     plt.show()
-    # raise NotImplementedError  # TODO: 删除该行，实现该函数功能
 
 
 if __name__ == "__main__":
