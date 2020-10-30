e58b6465e82634bbd9ce65096e6e8b26066d62b9
xia-yufeng <714811246@qq.com>
2020-10-31 00:28:35 +0800

    latest

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_random_matrix(m, n):
-    return np.random.randint(0,2,(m,n))
+    matrix = np.random.randint(0,2,(m,n))
+    return matrix
 
 
 def save_matrix(matrix, file_name):
